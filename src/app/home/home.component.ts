2a2c2cdb8df09c5718f42e77425138cdd0ef019c
Clement Carcenac <clementcarcenac@gmail.com>
2023-04-14 17:33:32 +0200

    Accordion

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
+import { Faq } from '../shared/faq';
+import { FAQ } from '../shared/faq-list';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './app-home.component.html' ,
-  styleUrls: ['./app-home.component.scss']
+  templateUrl: './home.component.html' ,
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+  questions!: Faq[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.questions = FAQ;
   }
 
 }
---
+++ src/app/shared/faq-list.ts
@@ -0,0 +1,39 @@
+import { Faq } from "./faq";
+
+export const FAQ: Faq[] = [
+    {
+        id: 0,
+        question: "Netflix, qu'est-ce que c'est ?",
+        response: "Netflix est un service de streaming qui propose une vaste sélection de séries, films, animes, documentaire et autres programmes sur des milliers d'appareils connectés à internet. Regardez tout ce que vous voulez, quand vous voulez, à un prix mensuel très attractif. Découvrez de nouveaux films et séries chaque semaine, il y en a pour tout les goûts !"
+    },
+    {
+        id: 1,
+        question: "Combien coûte Netflix ?",
+        response: "Regardez Netflix sur votre smartphone, tablette, Smart TV, ordinateur ou appareil de streaming, le tout pour un tarif mensuel fixe. Les offres vont de 5,99 € à 17,99 € par mois. Pas de contrat ni de frais supplémentaires."
+    },
+    {
+        id: 2,
+        question: "Qu'est-ce qui est différent dans une offre avec pub ?",
+        response: "Une offre avec pub vous permet de profiter de films et séries à un prix plus attractif. Vous pouvez regarder vos programmes préférés avec de courtes pauses pub (certaines restrictions sur l'emplacement et les appareils s'appliquent). Le téléchargement n'est pas pris en charge et quelques films et séries ne sont pas disponibles en raison de restrictions liées aux licences.."
+    },
+    {
+        id: 3,
+        question: "Où puis-je regarder Netflix ?",
+        response: "Netflix, c'est où vous voulez, quand vous voulez. Connectez-vous à votre compte pour regarder Netflix en ligne sur netflix.com depuis votre ordinateur ou tout appareil connecté à Internet avec l'application Netflix, comme les Smart TV, smartphones, tablettes, lecteurs de streaming et consoles de jeu. Vous pouvez aussi télécharger vos séries préférées avec l'application iOS, Android ou Windows 10. Téléchargez des titres pour les regarder sur votre appareil mobile, même sans connexion Internet. Emportez Netflix partout avec vous."
+    },
+    {
+        id: 4,
+        question: "Comment puis-je annuler mon offre ?",
+        response: "Netflix offre une grande souplesse. Pas de contrat compliqué. Sans engagement. Deux clics suffisent pour annuler votre compte en ligne. Pas de frais d'annulation : ouvrez ou fermez votre compte à tout moment."
+    },
+    {
+        id: 5,
+        question: "Que puis-je regarder sur Netlfix ?",
+        response: "Netflix propose un vaste catalogue comprenant notamment des longs-métrages, des documentaires, des séries, des animes et des programmes originaux Netflix primés. Regardez Netflix à volonté, quand vous le voulez."
+    },
+    {
+        id: 6,
+        question: "Est-ce que Netfix est adapté aux enfants ?",
+        response: "Netflix Jeunesse est inclus dans votre abonnement et offre un meilleur contrôle aux parents, ainsi qu'un espace dédié aux enfants, avec des films et des séries destinés à toute la famille. Les profils Jeunesse comportent des fonctionnalités de contrôle parental avec code PIN permettant de modifier la catégorie d'âge des contenus que vos enfants peuvent regarder et de bloquer des titres spécifiques."
+    }
+]
